b2bb55c6bc2d3e3fe7aa8218dd4eebc31940ae9b
noharra <noharra@666999.info>
2020-01-04 20:31:25 +0900

    hatuで訂正

---
+++ app/views/hatus/_hatu.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! hatu, :id, :body, :url, :sonota, :user_id, :created_at, :updated_at
+json.url hatu_url(hatu, format: :json)
---
+++ test/system/hatus_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class HatusTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit hatus_url
+  #
+  #   assert_selector "h1", text: "Hatu"
+  # end
+end
